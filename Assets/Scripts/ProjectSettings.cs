c993c418e853d71394447b13b90483935fc2b681
Nathan Harris <2nd.lt.harris@gmail.com>
2016-12-18 01:35:04 -0800

    Removed ESC shortcut for closing the pause menu; Added saving / loading of PlayerPrefs;

--- Assets/Scripts/ProjectSettings.cs
+++ Assets/Scripts/ProjectSettings.cs
@@ -11,4 +11,26 @@ public static class ProjectSettings
         MainMenu = 0,
         MainGameplay = 1
     }
+
+    public static class PlayerPrefs
+    {
+        public const string MASTER_VOLUME = "Master Volume";
+        public const string MUSIC_VOLUME = "Music Volume";
+        public const string SFX_VOLUME = "SFX Volume";
+        public const string AMBIENT_VOLUME = "Ambient Volume";
+    }
+
+    public static class AudioMixers
+    {
+        public static class Main
+        {
+            public static class Parameters
+            {
+                public const string MASTER_VOLUME = PlayerPrefs.MASTER_VOLUME;
+                public const string MUSIC_VOLUME = PlayerPrefs.MUSIC_VOLUME;
+                public const string SFX_VOLUME = PlayerPrefs.SFX_VOLUME;
+                public const string AMBIENT_VOLUME = PlayerPrefs.AMBIENT_VOLUME;
+            }
+        }
+    }
 }
