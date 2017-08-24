6993e131fa9d51fdbca2b574c6292786635e251a
Nathan Harris <nathanh@l4digital.com>
2017-08-24 14:02:06 -0700

    Import Animation Models
    - Add: InputController
    - Add: AnimationController
    - Add: Player prefab

---
+++ Assets/_core/Scripts/AnimationLookup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationLookup
+{
+    public struct Kach
+    {
+        public struct Floats
+        {
+            public const string HORIZONTAL = "Horizontal";
+            public const string VERTICAL = "Vertical";
+        }
+    }
+}
---
+++ Assets/_core/Scripts/PlayerInputController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour
+{
+    [SerializeField]
+    private float _movementSpeed;
+    [SerializeField]
+    private Animator _animator;
+
+    private void Start() 
+    {
+        if (_animator == null)
+        {
+            _animator = GetComponent<Animator>();
+        }
+    }
+
+    private void Update()
+    {
+		Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+		input = Vector3.ClampMagnitude(input, 1f) * Time.deltaTime * _movementSpeed;
+
+        _animator.SetFloat(AnimationLookup.Kach.Floats.HORIZONTAL, input.x);
+        _animator.SetFloat(AnimationLookup.Kach.Floats.VERTICAL, input.z);
+    }
+}
